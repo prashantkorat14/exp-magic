7453520554525c452c98efb883816f56e2182a74
Prashant Korat <prashantkorat14@gmail.com>
2020-01-29 11:06:39 +0530

    remove extra files

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,13 +1,5 @@
 import * as types from "../constants/ActionTypes";
 
-export const addTodo = text => ({ type: types.ADD_TODO, text });
-export const deleteTodo = id => ({ type: types.DELETE_TODO, id });
-export const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text });
-export const completeTodo = id => ({ type: types.COMPLETE_TODO, id });
-export const completeAllTodos = () => ({ type: types.COMPLETE_ALL_TODOS });
-export const clearCompleted = () => ({ type: types.CLEAR_COMPLETED });
-export const setVisibilityFilter = filter => ({ type: types.SET_VISIBILITY_FILTER, filter });
-
 export const addExperiment = data => ({ type: types.ADD_EXPERIMENT, payload: data });
 export const getAllExperiment = () => ({ type: types.GET_ALL_EXPERIMENT });
 
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,6 +1,4 @@
 import { combineReducers } from "redux";
-// import todos from './todos'
-// import visibilityFilter from './visibilityFilter'
 import exp from "./exp";
 
 import operation from "./operation";
